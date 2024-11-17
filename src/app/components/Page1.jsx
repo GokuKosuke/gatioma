043e9aaf313cf78f96ac66f6249155c19b18e2ae
GokuKosuke <oiwa20050313@icloud.com>
2024-11-17 12:44:26 +0900

    done

--- src/app/components/Page1.jsx
+++ src/app/components/Page1.jsx
@@ -1,12 +1,14 @@
 "use client"
 
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ChatIcon from '@mui/icons-material/Chat';
 
 function Page1() {
 
   const [clicked, setClicked] = useState(false);
+  const [randomImage, setRandomImage] = useState(null);
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -20,13 +22,36 @@ function Page1() {
         setClicked(false)
       }, 7200)
   }
+
+  const getRandomImage = () => {
+    const images = [
+      { src: "/blue.PNG", width: 250, height: 250, alt: "blue", className: "car blue" },
+      { src: "/white.png", width: 200, height: 200, alt: "white", className: "car white" },
+      { src: "/black.PNG", width: 200, height: 200, alt: "black", className: "car black" }
+    ];
+    const randomIndex = Math.floor(Math.random() * images.length); // 0〜2のランダムな整数を生成
+    return images[randomIndex];
+  };
+
+  useEffect(() => {
+    // 初回レンダリング時にランダム画像を設定
+    setRandomImage(getRandomImage());
+  }, []);
+
   return (
     <div>
         <div className={`topbar ${clicked ? "PavedTopbar" : ""}`}>
             <Image src="/fig.png" width={70} height={70} alt="fig" className={`fig ${clicked ? "figClicked": ""}`} onClick={handleClick}></Image>
-            <Image src="/blue.PNG" width={250} height={250} alt="fig" className={`fig blue ${clicked ? "fig2Clicked": ""}`}></Image>
-            <Image src="/white.png" width={200} height={200} alt="fig" className={`fig white ${clicked ? "fig2Clicked": ""}`}></Image>
-            <Image src="/black.PNG" width={200} height={200} alt="fig" className={`fig black ${clicked ? "fig2Clicked": ""}`}></Image>
+            {randomImage && (
+            <Image
+              src={randomImage.src}
+              width={randomImage.width}
+              height={randomImage.height}
+              alt={randomImage.alt}
+              className={`${randomImage.className} ${clicked ? "fig2Clicked" : ""}`} // clicked 状態によってクラスを変更
+              onClick={handleClick} // クリック時に状態を変更
+            />
+             )}
           <div className="title">
             <div className="GATIOMAE">
               <span className={`text text1 ${clicked ? "hidden" : "visible"}`}>OMAEGATI</span>
